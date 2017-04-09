ecbce6f67f6c07bb6f027fec9500e034ae053e30
Juhezi <juhezix@163.com>
2017-04-09 12:07:24 +0800

    add some introduction

--- src/com/Juhezi/data_structure_lib/sort/InsertSort.java
+++ src/com/Juhezi/data_structure_lib/sort/InsertSort.java
@@ -3,7 +3,8 @@ package com.juhezi.data_structure_lib.sort;
 import com.juhezi.data_structure_lib.util.JComparable;
 
 /**
- * <p>
+ * 插入排序
+ * 工作原理：通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
  * Created by qiao1 on 2017/2/13.
  */
 public class InsertSort {
@@ -26,7 +27,7 @@ public class InsertSort {
         for (int j = 1; j < len; j++) {
             key = array[j];
             i = j - 1;
-            while (i >= 0 && comparable.moreThan(array[i], key)) {
+            while (i >= 0 && comparable.moreThan(array[i], key)) {  //从后向前扫描，知道相应位置并插入
                 array[i + 1] = array[i];    //向后移一位
                 i = i - 1;
             }
